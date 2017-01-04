b7c3f94679e8337d7a8a7549f9d4acb76b44470d
Peter Springsteen <peter.springsteen@ge.com>
2017-01-04 18:38:08 -0500

    Accomodate base64 password, change metadata method

--- README.md
+++ README.md
@@ -6,10 +6,21 @@ This repo to contains groovy scripts to be used with Scriptler, with optional
 corresponding configuration files in `.json` format which contain metadata to be
 associated with a given script.
 
+*UPDATING TO FOLLOW METADATA CONVENTION AT:*
+https://github.com/jenkinsci/jenkins-scripts/tree/master/scriptler
+
 ## Exposed scripts
 | Name       | Description                              |
 |------------|------------------------------------------|
 | createUser | Script to allow remote user creation given a Username, Password, and Permission (optional) |
+
+### createUser
+| Parameter  | Description                                              |
+|------------|----------------------------------------------------------|
+| username   | *String* Username of the user to be added                |
+| password   | *String* Password of the user (supports Base64 encoding) |
+| base64     | *Boolean* Whether the password is Base64 encoded `true` or `false`|
+| permission | Can override default READ permission with 'ADMINISTER'   |
 
 ## Usage
 ### Scripts
--- scripts/createUser.groovy
+++ scripts/createUser.groovy
@@ -1,3 +1,11 @@
+/*** BEGIN META {
+ "name" : "createUser",
+ "comment" : "Script to allow remote user creation given a Username, Password, and Permission (optional)",
+ "parameters" : [ 'username', 'password', 'base64', 'permission' ],
+ "authors" : [
+ { name : "Peter Springsteen" }
+ ]
+ } END META**/
 import jenkins.model.*
 import java.util.logging.*
 import hudson.security.*
@@ -11,38 +19,48 @@ String randomPasswordGenerator() {
   return org.apache.commons.lang.RandomStringUtils.random(16, true, true)
 }
 
-if (!USERNAME || (USERNAME == '')) {
-  throw new Exception("USERNAME must be provided")
+String base64Decode(String decodeme) {
+  return new String(decodeme.decodeBase64())
+}
+
+if (!username || (username == '')) {
+  throw new Exception("'username' must be provided")
 }
 else {
-  if (!PASSWORD || (PASSWORD == '')) {
-    PASSWORD = randomPasswordGenerator()
-    println("New user $USERNAME created with random password: $PASSWORD. Please change.")
+  if (!password || (password == '')) {
+    password = randomPasswordGenerator()
+    println("New user $username created with random password: $password. Please change.")
+  } else {
+    if(base64 || (base64 != '')) {
+      if (Boolean.valueOf(base64.toLowerCase())) {
+        password = base64Decode(password)
+      }
+    }
   }
-  if (!PERMISSION || PERMISSION == '' || PERMISSION.lower() == 'read') {
+  if (!permission || permission == '' || permission.toLowerCase() == 'read') {
     permissionToUse = Jenkins.READ
-  } else if (PERMISSION.lower() == 'administer') {
+  } else if (permission.toLowerCase() == 'administer') {
     permissionToUse = Jenkins.ADMINISTER
   }
   else {
     permissionToUse = Jenkins.READ
   }
   def instance = Jenkins.getInstance()
-  def existingUser = hudson.model.User.get(USERNAME, false)
+  def existingUser = hudson.model.User.get(username, false)
   if (existingUser != null) println "INFO: Skipping account creation for existing user."
   AuthorizationStrategy strategy = instance.getAuthorizationStrategy()
   SecurityRealm sr = instance.getSecurityRealm();
   if (existingUser == null) {
-    sr.createAccount(USERNAME, PASSWORD)
+    sr.createAccount(username, password)
     instance.setSecurityRealm(sr)
   }
   try{
-    sr.loadUserByUsername(USERNAME)
+    sr.loadUserByUsername(username)
   } catch (UsernameNotFoundException e) {
     println "You need to add this user"
   }
   def currentStrategy = strategy.getDescriptor()
-  println strategy.add(permissionToUse, USERNAME)
+  println strategy.add(permissionToUse, username)
   instance.setAuthorizationStrategy(strategy)
 
   instance.save()
